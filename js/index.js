8cfc1d3b7f00134980a800e150b1b7ad3682d4bc
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2025-01-07 16:40:48 -0500

    practice modulus operator

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+// let number = 7
+// if(number % 2 == 0){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
 
+// let number = 13
+// if(number % 2 == 0){
+//     console.log("its an even Number");
+// }else{
+//     console.log("its an odd number");
+// }
